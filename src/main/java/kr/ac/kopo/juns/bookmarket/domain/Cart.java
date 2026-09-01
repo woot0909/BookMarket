d2a5635638d69582b64f9fd70d608c4f73bf4dd5
woot0909 <jeons0680@gmail.com>
2026-09-01 11:48:47 +0900

    13장

---
+++ src/main/java/kr/ac/kopo/juns/bookmarket/domain/Cart.java
@@ -0,0 +1,26 @@
+package kr.ac.kopo.juns.bookmarket.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@ToString
+public class Cart {
+    private String cartId;
+    private Map<String, CartItem> cartItems;
+    private BigDecimal grandTotal;
+
+    public Cart() {
+        cartItems = new HashMap<String, CartItem>();
+        grandTotal = new BigDecimal(0);
+    }
+
+    public Cart(String cartId) {
+        this();
+        this.cartId = cartId;
+    }
+}
---
+++ src/main/java/kr/ac/kopo/juns/bookmarket/domain/CartItem.java
@@ -0,0 +1,35 @@
+package kr.ac.kopo.juns.bookmarket.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Data
+@ToString
+public class CartItem {
+    private Book book;
+    private int quantity;
+    private BigDecimal totalPrice;
+
+    public CartItem(Book book) {
+        this.book = book;
+        quantity = 1;
+        totalPrice = book.getUnitPrice();
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+        updateTotalPrice();
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        updateTotalPrice();
+    }
+
+    public void updateTotalPrice(){
+        totalPrice = book.getUnitPrice().multiply(new BigDecimal(quantity));
+    }
+}
+
